98530233b5f2c798ae1159a61aa5043fc3f5f69e
Ritika <ritikarastogi2626@gmail.com>
2022-08-17 17:02:05 +0530

    Added difference file

---
+++ diff.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Difference
+{
+    public static void main(String[]args)
+    {
+        int a=12;
+        int b=3;
+        System.out.println("The difference is-"+(a-b));
+    }
+}
